d32783ee52c836cdf776971971afe59b33cc8b01
Bhavya B <bhavyaairody5@gmail.com>
2021-12-08 22:07:36 +0530

    First commit to GitHub

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+<head>
+     <title>Git Demo</title>
+     <script src="script.js" language="javascrpt"></script>
+</head>
+<body>
+     <h1>Git Hub Demo</h1>
+     <p>Welcome to my version</p>
+</body>
+</html>
